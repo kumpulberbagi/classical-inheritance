0640db7d3fd41eb1a1a232c12a0a7f42a20e2969
Alexander Hendrawan tjahja &   "name": null <pair+alexanderHT.dharmadi93@hacktiv8.com>
2016-10-03 13:39:50 +0700

    animals inheritance

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,71 @@
 "use strict"
+
+class SuperPowered{
+
+  hulk_smash(){
+    // console.log("Pukulan maut!");
+    return "Pukulan maut!"
+  }
+
+  fly_rocket(){
+    // console.log("terbang !");
+    return "terbang !"
+  }
+}
+
+class Animal {
+  constructor(nama, jumlah_kaki, darah_panas) {
+    this._nama = nama
+    this._jumlah_kaki = jumlah_kaki
+    this._darah_panas = darah_panas
+    this.superpower = new SuperPowered();
+  }
+}
+
+class Mamal extends Animal {
+  constructor(nama, jumlah_kaki, darah_panas) {
+    super(nama, jumlah_kaki, darah_panas)
+
+  }
+} // super class
+
+class Reptile extends Animal{
+  constructor(nama, jumlah_kaki, darah_panas) {
+    super(nama, jumlah_kaki, darah_panas)
+
+  }
+} // super class
+
+class Amphibian extends Reptile {
+  constructor(nama, jumlah_kaki, darah_panas) {
+    super(nama, jumlah_kaki, darah_panas)
+
+  }
+} // sub set dari class reptile
+
+class Primate extends Mamal{
+  constructor(nama, jumlah_kaki, darah_panas) {
+    super(nama, jumlah_kaki, darah_panas)
+
+  }
+} // subset dari mamal
+
+class Frog extends Amphibian{
+  constructor(nama, jumlah_kaki, darah_panas) {
+    super(nama, jumlah_kaki, darah_panas)
+
+  }
+} //
+
+class Chimpanzee extends Primate{
+  constructor(nama, jumlah_kaki, darah_panas) {
+    super(nama, jumlah_kaki, darah_panas)
+    // this.superpower
+  }
+}
+// var binatang = new Animal()
+// var primate = new Primate()
+var harambe = new Chimpanzee("harambe", 2, true);
+console.log(harambe.superpower.hulk_smash());
+console.log(harambe);
+// harambe.superpower.hulk_smash();
